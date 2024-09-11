f5b41fd74449b455d3d56c3922ef08bec3029c7f
Mɾ Nƚαɳԃσ Oϝƈ <chisayantando61@gmail.com>
2024-09-11 12:03:44 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-var SESSION_ID = ''
+var SESSION_ID = 'QUEENELISA;;;==AMSB1SINDNlJjWyQTdzIHTllVWulEcMFFTrNXRXVzUOJFO4ZFdsBVQwgFTjMERaVUMzp0cpwd=961546482329'
 global.AI_PASSWORD = 'king-nima-gpt-bot'
 global.SESSION_ID = process.env.SESSION_ID || SESSION_ID 
 
